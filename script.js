546c8eceff560ac863a7b94d29eede41cde5a6bd
igs111 <iqciuplay@gmail.com>
2026-05-25 13:50:24 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Możesz dodać interaktywne efekty, np. animacje lub przejścia
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Portfolio Igora Godziewskiego załadowane.");
+});
