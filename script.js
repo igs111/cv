5ffacb7ec521e891bca3c3fab7aaf1b332edf9d5
igs111 <iqciuplay@gmail.com>
2026-05-25 14:15:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-  /* POWIĘKSZANIE ZDJĘĆ */
   const overlay = document.getElementById("overlay");
   const overlayImg = document.getElementById("overlayImg");
   const closeBtn = document.getElementById("closeBtn");
@@ -20,25 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
     if (e.target === overlay) overlay.style.display = "none";
   });
 
-  /* ANIMACJE SCROLLOWE */
-  const reveals = document.querySelectorAll(".reveal");
-
-  function revealOnScroll() {
-    const trigger = window.innerHeight * 0.85;
-
-    reveals.forEach(el => {
-      const top = el.getBoundingClientRect().top;
-      if (top < trigger) el.classList.add("active");
-    });
-  }
-
-  window.addEventListener("scroll", revealOnScroll);
-  revealOnScroll();
-
-  /* PODŚWIETLANIE KURSORA */
+  // efekt rozmazania wokół kursora
   document.addEventListener("mousemove", (e) => {
     document.body.style.setProperty("--cursorX", e.clientX + "px");
     document.body.style.setProperty("--cursorY", e.clientY + "px");
-    document.body.style.setProperty("--cursorX", e.clientX + "px");
+    document.body.style.setProperty("transform", `translate(${e.clientX}px, ${e.clientY}px)`);
+    document.body::before;
   });
 });
